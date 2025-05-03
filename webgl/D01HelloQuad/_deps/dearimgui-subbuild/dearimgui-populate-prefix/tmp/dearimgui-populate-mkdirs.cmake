92fd2e187f8ed47baec99dca405073b83f8ef8d9
pokemon0819 <pokemon819@naver.com>
2025-05-04 00:57:02 +0900

    HelloQuad (1)

---
+++ webgl/D01HelloQuad/_deps/dearimgui-subbuild/dearimgui-populate-prefix/tmp/dearimgui-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-src"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-build"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/tmp"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/src/dearimgui-populate-stamp"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/src"
+  "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/src/dearimgui-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/src/dearimgui-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/GIthub Code/cs250-graphics-fun-SeunghyeonSong/build/web-debug-on-windows/_deps/dearimgui-subbuild/dearimgui-populate-prefix/src/dearimgui-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
